3b4621c9a078927760757adc88676347855b1d66
LX1993728 <liuxun1993&%$#@>
2019-05-26 23:19:24 +0800

    +++ ADD  OSGI IN SPRINGBOOT

---
+++ part_00/springboot_osgi/springboot-osgi-example/src/main/java/com/liuxun/osgi/SpringbootOsgiExampleApplication.java
@@ -0,0 +1,13 @@
+package com.liuxun.osgi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootOsgiExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootOsgiExampleApplication.class, args);
+    }
+
+}
