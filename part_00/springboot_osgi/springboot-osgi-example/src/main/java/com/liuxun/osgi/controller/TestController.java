ea7c632c6938f5b82862d9db699d406ed8c9af35
LX1993728 <liuxun1993&%$#@>
2019-05-30 00:06:43 +0800

    +++ ADD  OSGI IN SPRINGBOOT Test +++++

--- part_00/springboot_osgi/springboot-osgi-example/src/main/java/com/liuxun/osgi/controller/TestController.java
+++ part_00/springboot_osgi/springboot-osgi-example/src/main/java/com/liuxun/osgi/controller/TestController.java
@@ -20,24 +20,15 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() throws Exception{
-        ServiceLoader<FrameworkFactory> ffs = ServiceLoader.load(FrameworkFactory.class);
-        FrameworkFactory ff = ffs.iterator().next();
-        Map<String,String> config = new HashMap<>();
-        // add some params to config ...
-        Framework fwk = ff.newFramework(config);
-        fwk.init();
-        fwk.start();
-        BundleUtils.framework = fwk;
+        final Framework framework = BundleUtils.framework;
+        BundleContext context = framework.getBundleContext();
 
-
-//        final Framework framework = BundleUtils.framework;
-//        BundleContext context = framework.getBundleContext();
-//
 //        Bundle provider = context.installBundle("file:/Users/liuxun//Downloads/test/bundles/springdm-provider-test-1.0.jar");
-//        provider.start();
-//        ServiceReference<?>[] registeredServices = provider.getRegisteredServices();
-//        System.out.println(registeredServices.length);
-//        provider.stop();
+        Bundle provider = context.installBundle("file:/c:/Users/Administrator/Desktop/bundles/springdm-provider-test-1.0.jar");
+        provider.start();
+        ServiceReference<?>[] registeredServices = provider.getRegisteredServices();
+        System.out.println(registeredServices.length);
+        provider.stop();
         return "测试是否能正常运行";
     }
 
--- part_00/springdm-osgi/springdm-provider-test/src/main/java/springdm/provider/test/serviceImpl/HelloServiceImpl.java
+++ part_00/springdm-osgi/springdm-provider-test/src/main/java/springdm/provider/test/serviceImpl/HelloServiceImpl.java
@@ -6,7 +6,6 @@ import springdm.provider.test.HelloService;
 
 public class HelloServiceImpl implements HelloService {
 
-    @Override
     public void sayHello(String name) {
         System.out.println("Hello "+name+" !!!!!");
     }
