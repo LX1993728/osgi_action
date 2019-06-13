bedc5643387113471e355418d4547d30e032af66
LX1993728 <2652790899@qq.com>
2019-06-13 18:00:46 +0800

    ++++ ａlittle　update　 ++++++++

--- part_00/1.springboot_osgi/springboot-osgi-web/src/main/java/com/liuxun/osgi/controller/TestController.java
+++ part_00/1.springboot_osgi/springboot-osgi-web/src/main/java/com/liuxun/osgi/controller/TestController.java
@@ -17,41 +17,41 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() throws Exception {
-
+        // 测试OSGI服务动态选择
         MyAnnotitationService service = BundleUtils.findServiceDynamic(MyAnnotitationService.class);
-        if (service != null){
+        if (service != null) {
             String dateStr = service.getPatternDateStr(new Date());
             System.out.println(dateStr);
-            return  dateStr;
+            return dateStr;
         }
         return null;
     }
 
     @GetMapping("/test1")
     public Object test1() throws Exception {
-
+        // 测试根据Service的Metadata属性进行服务查找
         Map<String, Object> meta = new HashMap<>();
-        meta.put("version","1.0");
+        meta.put("version", "1.0");
         MyAnnotitationService service = BundleUtils.findServiceByComponentAttr(MyAnnotitationService.class, meta);
-        if (service != null){
+        if (service != null) {
             String dateStr = service.getPatternDateStr(new Date());
             System.out.println(dateStr);
-            return  dateStr;
+            return dateStr;
         }
         return null;
     }
 
     @GetMapping("/test2")
     public Object test2(String version) throws Exception {
+        // 测试根据Bundle version属性查找需要的service实现
         BundleContext context = BundleUtils.bundleContext;
-
         String bundleVersion = version == null ? "1.0" : version;
         MyAnnotitationService service = BundleUtils.findServiceByBundleVersion(MyAnnotitationService.class, bundleVersion);
 
-        if (service != null){
+        if (service != null) {
             String dateStr = service.getPatternDateStr(new Date());
             System.out.println(dateStr);
-            return  dateStr;
+            return dateStr;
         }
         return null;
     }
